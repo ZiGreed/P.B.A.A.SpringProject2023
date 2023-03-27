3b5310013125c2f75262415be425c9b7f6088300
RytasSt <119342489+RytasSt@users.noreply.github.com>
2023-03-27 19:05:55 +0300

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 function Dashboard() {
     return ( 
         <>
-        <h2>dashboard</h2>
-        <Link to={"/expenses/"} ><button>Expenses</button></Link>
+
         </>
      );
 }
