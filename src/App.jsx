dae21285af6a53bbd08966228c5ff78751293af3
DeimanteRag <d.ragauskaitee@gmail.com>
2023-03-29 19:54:50 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Incomes from "./components/Incomes";
 import Expenses from "./components/Expenses";
+import AddIncomes from "./components/AddIncomes";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<MainPage />}>
+            <Route path="addIncomes" element={<AddIncomes/>} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="incomes" element={<Incomes />} />
             <Route path="expenses" element={<Expenses />} />
