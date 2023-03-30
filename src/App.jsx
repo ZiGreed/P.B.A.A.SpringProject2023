91486317766d4553929e4fe6a19ed50428a5de98
RytasSt <119342489+RytasSt@users.noreply.github.com>
2023-03-30 04:25:34 +0300

    Read Expenses + css almost done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import MainPage from "./components/MainPage";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Incomes from "./components/Incomes";
-import Expenses from "./components/Expenses";
+import AddExpenses from "./components/AddExpenses";
+import ReadExpenses from "./components/ReadExpenses";
 
 
 function App() {
@@ -16,7 +17,8 @@ function App() {
           <Route path="/" element={<MainPage />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="incomes" element={<Incomes />} />
-            <Route path="expenses" element={<Expenses />} />
+            <Route path="expenses" element={<ReadExpenses />} />
+            <Route path="addexpense" element={<AddExpenses />} />
           </Route>
         </Routes>
       </Layout>
