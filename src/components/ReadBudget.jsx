0df64b4c8cd97dcefd545d1cd155dd0e1e61dd4e
RytasSt <119342489+RytasSt@users.noreply.github.com>
2023-04-19 19:26:38 +0300

    2

--- src/components/ReadBudget.jsx
+++ src/components/ReadBudget.jsx
@@ -1,16 +1,54 @@
 import { Link } from "react-router-dom";
 import Vector from "./../assets/images/Vector.svg";
+import { RiDeleteBinLine, RiEdit2Line } from "react-icons/ri";
 
 
 function ReadBudget() {
     return ( 
         <>
-        <p>
-            Maistas
-            Pramogos
-            Buitis
-            Kita
-        </p>
+        <div className="readExpensejsx">
+      <div className="cardsWrapper">
+        <div className="cardsContainerBorder">
+          <div className="cardsContainer overflowHidden">
+            
+        <div className="card">
+          <div className="cardIndoWrapper">
+              <div>Maistas</div>
+          </div>
+          <div className="ButtonsContainer">
+          <div className="buttonIcons">
+            {/* <Link to={"/editexpenses/" + expense.id} className="buttonIcons"> */}
+              <RiEdit2Line size={30} />
+            {/* </Link> */}
+          </div>
+          <div className="buttonIcons">
+            <RiDeleteBinLine
+              size={30}
+              // onClick={() => {
+              //   deleteHandler(expense, deleteExpense);
+              // }}
+            />
+          </div>
+        </div>
+        </div>
+        <div className="card">
+          <div className="cardIndoWrapper">
+              <div>Pramogos</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="cardIndoWrapper">
+              <div>Buitis</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="cardIndoWrapper">
+              <div>Kita</div>
+          </div>
+        </div>
+            
+          </div>
+        </div>
         <div className="LinkWrapper">
           <Link to="/addbudget/" className="LinkButton">
             <button className="buttonAdd">
@@ -18,6 +56,8 @@ function ReadBudget() {
             </button>
           </Link>
         </div>
+      </div>
+    </div>
         </>
      );
 }
